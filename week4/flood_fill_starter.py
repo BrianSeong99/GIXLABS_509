aa74e3a9e7458f49ec280485ca42388110d7f012
Cherry <white.cherryblossom298@gmail.com>
2022-10-31 00:22:46 -0700

    finished assignment

--- week4/flood_fill_starter.py
+++ week4/flood_fill_starter.py
@@ -26,6 +26,24 @@ def flood_fill(input_board: List[str], old: str, new: str, x: int, y: int) -> Li
     """
 
     # Implement your code here.
+    if x >= len(input_board) or x < 0:
+        return input_board
+    if y >= len(input_board[x]) or y < 0:
+        return input_board
+    if input_board[x][y] != old:
+        return input_board
+    
+    row = list(input_board[x])
+    row[y] = new
+    input_board[x] = ''.join(row)
+
+    input_board = flood_fill(input_board, old, new, x+1, y)
+    input_board = flood_fill(input_board, old, new, x-1, y)
+    input_board = flood_fill(input_board, old, new, x, y+1)
+    input_board = flood_fill(input_board, old, new, x, y-1)
+
+    return input_board
+
 
 
 modified_board = flood_fill(input_board=board, old=".", new="~", x=5, y=12)
